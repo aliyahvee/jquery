f8263354413089cafa0cfe0b129967711794eb53
Aliyah Veal <Aliyah@Aliyahs-MBP-2.home>
2018-02-17 17:20:39 -0500

    updated script

--- main.js
+++ main.js
@@ -1,48 +1,54 @@
 //Add in external javascript file
 $(document).ready(function(){ //verifies all HTML loaded
-  $("#hi-gandhi").on("click", function (e) {e.preventDefault(); //disable the button's default behavior
-$('#para1').addClass('highlight');
- // Note no # or . for class being added
-$('#hi-gandhi').prop('disabled', true);
-});
 
-$("#hi-maya").on("click", function (e) {e.preventDefault(); //disable the button's default behavior
-$('#para2').addClass('highlight');
-$('hi-maya').prop('disabled', true);
-});
+  $("#hi-gandhi").on("click", function (e) {
+        e.preventDefault(); //disable the button's default behavior
+      $('#para1').addClass('highlight'); // Note no # or . for class being added
+      $('#hi-gandhi').prop('disabled', true);
+    });
 
-$('#style-hed').on("click", function (e) {e.preventDefault(); //disable the button's default behavior
-$("h3").addClass("active");
-$('#style-hed').prop('disabled', true);
-});
+$("#hi-maya").on("click", function (e) {
+       e.preventDefault(); //disable the button's default behavior
+    $('#para2').addClass('highlight');
+    $('hi-maya').prop('disabled', true);
+    });
 
-$('#style-multi').on("click", function (e) {e.preventDefault(); //disable the button's default behavior
+  $('#style-hed').on("click", function (e) {
+        e.preventDefault(); //disable the button's default behavior
+      $("h3").addClass("active");
+      $('#style-hed').prop('disabled', true);
+  });
+
+  $('#style-multi').on("click", function (e) {
+        e.preventDefault(); //disable the button's default behavior
 //console.log('hello');
-$('#style-multi').prop('disabled', true);
-});
+    $('#style-multi').prop('disabled', true);
+  });
 
-$('style-multi').on("click", function (e) {e.preventDefault(); //disable the button's default behavior
-$('#para1, #para2').addClass("addBorder higlight"); //adding multiple classes to several elements
-$('.authors').addClass('someStyling somePadding'); //adding multiple classes to single element
-});
+  $('style-multi').on("click", function (e) {
+       e.preventDefault(); //disable the button's default behavior
+      $('#para1, #para2').addClass("addBorder higlight"); //adding multiple classes to several elements
+      $('.authors').addClass('someStyling somePadding'); //adding multiple classes to single element
+    });
+$("#swap-div").on("click", function (e) {
+      e.preventDefault(); //disable the button's default behavior
+    $(".demo").addClass('newDiv');
+    $("#swap-div").prop('disabled', true);
+    });
 
-$("#swap-div").on("click", function (e) {e.preventDefault(); //disable the button's default behavior
-$(".demo").addClass('newDiv');
-$("#swap-div").prop('disabled', true);
-});
-
-$("#reset-all").on("click", function (e) {e.preventDefault(); //disable the button's default behavior
+  $("#reset-all").on("click", function (e) {
+        e.preventDefault(); //disable the button's default behavior
 
 $('.demo').removeClass('newDiv').addClass('demo');
-//removeClass() works when chained with addClass()
-$('.head-txt, #para1, #para2').removeClass('addBorder');
-//remove border from everything;
-$('authors').removeClass('someStyling'
+    //removeClass() works when chained with addClass()
+    $('.head-txt, #para1, #para2').removeClass('addBorder');
+    //remove border from everything;
+    $('authors').removeClass('someStyling'
 'somePadding').addClass('authors');
 //removeClass() multiple classes
-$('#para2, #para1, .head-txt').removeClass('highlight').addClass('no-highlight');
-$("h3").removeClass("active");
-$("button").prop('disabled', false);
+    $('#para2, #para1, .head-txt').removeClass('highlight').addClass('no-highlight');
+    $("h3").removeClass("active");
+  $("button").prop('disabled', false);
 
-});
+  });
 }); //closes all the functions
